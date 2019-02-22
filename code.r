661e6bbdca80b1016ff1edf0a7dcb00e72cedc6a
Andira Putri <35618440+diramputri@users.noreply.github.com>
2019-02-21 20:23:36 -0500

    Update code.r

--- code.r
+++ code.r
@@ -1,17 +1,43 @@
 ## testing that R works
 
 install.packages("ISLR")
+install.packages("mlbench")
 library(ISLR)
+library(mlbench)
+
+# ------------------------------------------------------------------------------------- #
 
 ## Simple Linear Regression
+# We will predict Boston Housing prices
+
+# load data set from mlbench library
+data=BostonHousing
+#view data
+View(data)
+# what are our variables?
+?BostonHousing
+
+# rm is number of rooms in home
+# maybe that would be a good predictor?
 
 # lm(response~predictor,data=...)
+# our outcome is medv, the median value of homes in USD 1000's
+
+lin_reg=lm(medv~rm,data=data)
 
 # summary() gives our coefficients, residuals, and test statistics
 
+summary(lin_reg)
+
 # plot our line of best fit on our data
 
-data(car)
+plot(data$rm,data$medv)
+abline(lin_reg)
+
+# residual plots
+# plot(your lm() function)
+
+plot(lin_reg)
 
 # ------------------------------------------------------------------------------------- #
 
@@ -23,4 +49,25 @@ data(car)
 
 # ------------------------------------------------------------------------------------- #
 
+## Ridge Regression
+
+# ------------------------------------------------------------------------------------- #
+
+## Lasso
+
+# ------------------------------------------------------------------------------------- #
+
 ## Logistic Regression
+
+# ------------------------------------------------------------------------------------- #
+
+## Train/Test Split
+
+# ------------------------------------------------------------------------------------- #
+
+## k-Fold Cross Validation
+
+# ------------------------------------------------------------------------------------- #
+
+## Leave One Out Cross Validation (LOOCV)
+
