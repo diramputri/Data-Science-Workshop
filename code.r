60fa3e2fa4026f36e49874206505f18215fae55d
Andira Putri <35618440+diramputri@users.noreply.github.com>
2019-02-21 19:51:08 -0500

    Update code.r

--- code.r
+++ code.r
@@ -1,1 +1,26 @@
+## testing that R works
 
+install.packages("ISLR")
+library(ISLR)
+
+## Simple Linear Regression
+
+# lm(response~predictor,data=...)
+
+# summary() gives our coefficients, residuals, and test statistics
+
+# plot our line of best fit on our data
+
+data(car)
+
+# ------------------------------------------------------------------------------------- #
+
+## Multiple Linear Regression
+
+# lm(response~predictor1+predictor2+...+predictork, data=...) for just a subset of predictors
+
+# lm(response~., data=...) to include ALL predictors in the data set
+
+# ------------------------------------------------------------------------------------- #
+
+## Logistic Regression
